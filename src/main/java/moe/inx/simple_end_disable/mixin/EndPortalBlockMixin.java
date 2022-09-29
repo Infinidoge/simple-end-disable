eab297cb17bc1404bcd24158555d1af20b427474
Infinidoge <infinidoge@doge-inc.net>
2022-09-28 23:05:32 -0400

    nit: remove unnecessary comments
    They were included from modifying the mixin from progression

--- src/main/java/moe/inx/simple_end_disable/mixin/EndPortalBlockMixin.java
+++ src/main/java/moe/inx/simple_end_disable/mixin/EndPortalBlockMixin.java
@@ -15,22 +15,13 @@ import net.minecraft.util.Formatting;
 @Mixin(EndPortalBlock.class)
 public abstract class EndPortalBlockMixin extends BlockWithEntity {
 
-	/* Generic constructor */
 	protected EndPortalBlockMixin(Settings settings) {
 		super(settings);
 	}
 
-	/*
-	 * Redirect the onEntityCollision() method from the EndPortalBlock class to our
-	 * custom method below.
-	 */
 	@Redirect(method = "onEntityCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;moveToWorld(Lnet/minecraft/server/world/ServerWorld;)Lnet/minecraft/entity/Entity;"))
 	private Entity simple_end_disable$disableEndPortal(Entity entity, ServerWorld serverWorld) {
-		/* Check to see if the dimension is enabled or disabled */
 		if (entity instanceof PlayerEntity) {
-			/*
-			 * Create and send the message to send to players if the dimension is disabled.
-			 */
 			Text playerMsg = Text.literal("The End is disabled").formatted(Formatting.RED, Formatting.BOLD);
 			((PlayerEntity) entity).sendMessage(playerMsg, true);
 		}
